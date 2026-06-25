f686d40ad89947df71a79e15b8103dbd5d6cdc40
Gianluca Lambruschini <gianluca.lambruschini@gmail.com>
2026-06-25 18:12:41 +0000

    Archivo main donde se instancia el grafo junto a funciones que mejoran el mismo

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import re
 import uuid
 import sqlite3
 from pathlib import Path
-from typing import TypedDict
 from dotenv import load_dotenv
 load_dotenv()
 from langchain_core.messages import HumanMessage
-from langgraph.checkpoint.memory import MemorySaver
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph import MessagesState, StateGraph, END
 from graph.nodes import run_agent_reasoning, run_agent_reflection, run_format_output, run_sumarize, safe_tool_node
@@ -43,39 +41,38 @@ def get_last_content(messages:list) -> str:
 def clean_output(text: str) -> str:
     return re.sub(r"<think>.*?</think>", "", text, flags=re.DOTALL).strip()
 
-graph_flow = StateGraph(MessagesState)
-
-graph_flow.add_node(AGENT_REASON, run_agent_reasoning)
-graph_flow.set_entry_point(AGENT_REASON)
-graph_flow.add_node(FORMATTER, run_format_output)
-graph_flow.add_node(SUMARIZER, run_sumarize)
-graph_flow.add_node(REFLECT, run_agent_reflection)
-graph_flow.add_node(ACT, safe_tool_node)
-graph_flow.add_conditional_edges(AGENT_REASON, should_continue, {
-    END:END,
-    ACT:ACT})
-graph_flow.add_edge(ACT, REFLECT)
-graph_flow.add_conditional_edges(REFLECT, after_reflect, {
-    FORMATTER:FORMATTER, 
-    SUMARIZER:SUMARIZER,
-    AGENT_REASON:AGENT_REASON})
-graph_flow.add_edge(SUMARIZER, FORMATTER)
-graph_flow.add_edge(FORMATTER, END)
-
-sqlite_folder = Path("sqlite")
-sqlite_folder.mkdir(exist_ok=True)
-checkpoint_path = sqlite_folder / "checkpoints.sqlite"
-
-with SqliteSaver.from_conn_string(str(checkpoint_path)) as memory:
+def build_graph():
+    graph_flow = StateGraph(MessagesState)
+    graph_flow.add_node(AGENT_REASON, run_agent_reasoning)
+    graph_flow.set_entry_point(AGENT_REASON)
+    graph_flow.add_node(FORMATTER, run_format_output)
+    graph_flow.add_node(SUMARIZER, run_sumarize)
+    graph_flow.add_node(REFLECT, run_agent_reflection)
+    graph_flow.add_node(ACT, safe_tool_node)
+    graph_flow.add_conditional_edges(AGENT_REASON, should_continue, {
+        END:END,
+        ACT:ACT})
+    graph_flow.add_edge(ACT, REFLECT)
+    graph_flow.add_conditional_edges(REFLECT, after_reflect, {
+        FORMATTER:FORMATTER, 
+        SUMARIZER:SUMARIZER,
+        AGENT_REASON:AGENT_REASON})
+    graph_flow.add_edge(SUMARIZER, FORMATTER)
+    graph_flow.add_edge(FORMATTER, END)
+    sqlite_folder = Path("sqlite")
+    sqlite_folder.mkdir(exist_ok=True)
+    checkpoint_path = sqlite_folder / "checkpoints.sqlite"
     
-    graph = graph_flow.compile(checkpointer=memory)
-    graph.get_graph().draw_mermaid_png(output_file_path="graph_flow.png")
+    conn = sqlite3.connect(str(checkpoint_path), check_same_thread=False)
+    memory = SqliteSaver(conn)
+    return graph_flow.compile(checkpointer=memory)
 
-    if __name__ == "__main__":
-        thread = {"configurable": {"thread_id": str(uuid.uuid4())}}
+if __name__ == "__main__":
+    graph = build_graph()
+    thread = {"configurable": {"thread_id": str(uuid.uuid4())}}
     
-        while True:
-            user_text = input("Sobre qué querés hablar?: ")
-            res = graph.invoke({"messages": [HumanMessage(content=user_text)]}, config=thread)
-            print(clean_output(get_last_content(res["messages"])))
+    while True:
+        user_text = input("Sobre qué querés hablar?: ")
+        res = graph.invoke({"messages": [HumanMessage(content=user_text)]}, config=thread)
+        print(clean_output(get_last_content(res["messages"])))
             
